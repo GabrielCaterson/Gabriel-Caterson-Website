6411431e220351dbecb1d86cc6896b0244a21c2a
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2022-11-03 05:59:12 -0400

    Added more to about-me.

--- src/App.js
+++ src/App.js
@@ -19,8 +19,17 @@ function App() {
         Gabriel Caterson
       </h1>
 
+      <section>
+        <picture className="socials-image">
+          { /*<img src="./images/socials/GitHub.png"></img>*/ }
+        </picture>
+        <picture className="socials-image"></picture>
+        <picture className="socials-image"></picture>
+
+      </section>
+
       <section className="about-me">
-        I am a full stack computer programmer based in Florida. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        I am a full stack computer programmer based in Lithia, Florida. I enjoy playing piano and reading academic philosophy papers and books on philosophy of mind, ethics, animal rights, and the future. 
       </section>
 
       <section className="card-box">
